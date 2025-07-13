5df36e0d7cccfb4f8027eb362f5db85b7507c469
염태웅 <yeomtaeung@yeomtaeung-ui-MacBookPro-2.local>
2025-07-13 14:55:51 +0900

    Update base href to fix GitHub Pages routing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1305113372"
+    serviceWorkerVersion: "2170750619"
   }
 });
